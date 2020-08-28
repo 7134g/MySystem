2a4aebd82963d0ac92ab9c8f26f78d960ca2c6ec
liaoxincheng <987712030@qq.com>
2020-08-28 21:01:59 +0800

    setting blog route

--- app/blog/route/extrance.go
+++ app/blog/route/extrance.go
@@ -17,11 +17,22 @@ func Blogroute(r *gin.Engine) {
 		bg.POST("user/login", blogApi.Login)
 		bg.POST("user/register", blogApi.UserRegister)
 
+		bg.GET("home", blogApi.Home)
+		bg.GET("ShowArticles", blogApi.ShowArticle)
+		bg.GET("classfication", blogApi.Classfication)
+		bg.GET("TimeLine", blogApi.TimeLine)
+		bg.GET("JianLi", blogApi.JianLi)
+
 		// 需要登录保护的
 		authed := bg.Group("/")
 		authed.Use(middleware.AuthRequired())
 		{
-
+			// 创建文章
+			bg.POST("NewArticle", blogApi.CreateArticle)
+			// 删除文章
+			bg.DELETE("DelArticle", blogApi.DelArticle)
+			// 更新文章
+			bg.POST("ChangeArticle", blogApi.UpdateArticle)
 		}
 
 	}
